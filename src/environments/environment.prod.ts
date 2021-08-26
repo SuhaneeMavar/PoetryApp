7bf780119fe6332c6dc00c6574ca6165a0e6be6d
SDK2021 <77728099+SDK2021@users.noreply.github.com>
2021-08-26 20:53:00 +0530

    initial

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
-
+import {PORT} from '../../server/config/config.js'
 export const environment = {
   production: true
 };
-export const BASE_URL=`https://poetries.herokuapp.com:/`
+export const BASE_URL=`https://poetries.herokuapp.com:${PORT}/`
